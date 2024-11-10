2ab96e5e56047b199f46c0e7888c73b7a692ea63
wemmy6673 <okunolawemzy@gmail.com>
2024-11-10 09:48:27 +0100

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,26 @@
-import { TypeAnimation } from 'react-type-animation';
 import Wemi from '../images/wemi.jpeg';
+
 const Hero = () => {
-
-    return(
-        <div className="bg-slate-100 p-16 w-full flex flex-col space-y-6 justify-center items-center" style={{ borderBottomLeftRadius: '25%', borderBottomRightRadius: '25%' }}>
-            <img src={Wemi} alt="" width="" height="" className='rounded-full w-56 h-56 object-cover border-4 border-white' />
-            <div className="w-full text-center">
-                <TypeAnimation
-                     sequence={[
-                     'Hi there! I\'m Wemmy',
-                     1000,
-                        'I am a Front-end Web developer',
-                        1000,
-                        'I build user friendly web applications',
-                        1000,
-                        'Let\'s give your brand a creative start right away',
-                         1000,
-                    ]}
-                 wrapper="span"
-                 speed={50}
-                repeat={Infinity}
-                className="font-semibold text-lg md:text-4xl text-gradient-to-r from-black to-gray-600"
-                />
-         </div>
-         <button className='bg-black text-white rounded-full p-3'>Learn more</button>
+  return (
+    <div
+      className="bg-slate-100 p-16 w-full flex flex-col space-y-6 justify-center items-center"
+      style={{ borderBottomLeftRadius: '25%', borderBottomRightRadius: '25%' }}
+    >
+    
+        <img
+          src={Wemi}
+          alt="Wemi"
+          className="rounded-full w-56 h-56 object-cover border-4 border-white"
+        />
+        <div className="w-full text-center">
+          <p className='text-lg md:text-3xl font-semibold'>Hi there! Wemmy Here</p>
+          <p className='text-lg md:text-3xl font-semibold'>I am a front end developer</p>
+          <p className='text-lg md:text-3xl font-semibold'>I build user friendly web applications</p>
         </div>
-
-    )
-}
+      
+      <button className="bg-black text-white rounded-full p-3">Learn more</button>
+    </div>
+  );
+};
 
 export default Hero;
