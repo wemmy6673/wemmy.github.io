d8f74207be6fe521496afebd057ad9e2cb59a2b4
wemmy6673 <okunolawemzy@gmail.com>
2024-11-22 02:13:15 +0100

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,24 +4,24 @@ const Skills = () => {
         <div className="w-full">
         <h1 className='px-20 text-3xl text-center md:text-left'>Stack</h1>
         <div className="grid grid-cols-2 gap-3 p-8 md:gap-6 md:py-6 md:px-20 md:w-[40%]">
-            <div className="bg-blue-400 px-3 py-1 rounded-lg">
+            <div className="bg-blue-400 px-3 py-3  rounded-lg">
                 <p className="text-lg text-white">CSS</p>
 
             </div>
-            <div className="bg-slate-300 px-3 py-1 rounded-lg">
+            <div className="bg-slate-300 px-3 py-3 rounded-lg">
                 <p className="text-lg text-black">TAILWIND</p>
 
             </div>
-            <div className="bg-green-500 px-3 py-1 rounded-lg">
+            <div className="bg-green-500 px-3 py-3 rounded-lg">
                 <p className="text-lg text-white">JAVASCRIPT</p>
 
             </div>
-            <div className="bg-orange-600 px-3 py-1 rounded-lg">
+            <div className="bg-orange-600 px-3 py-3 rounded-lg">
                 <p className="text-lg text-white">REACT</p>
 
             </div>
-            <div className="bg-black px-3 py-1 rounded-lg">
-                <p className="text-lg text-white">NEXT</p>
+            <div className="bg-black px-3 py-3 rounded-lg">
+                <p className="text-lg text-white">HTML</p>
 
             </div>
 
