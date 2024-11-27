b6e9cf60bdc8ec96e72d064b12e19b603345b907
wemmy6673 <okunolawemzy@gmail.com>
2024-11-27 09:30:13 +0100

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,27 +9,27 @@ const Skills = () => {
         <div className="w-full py-20">
         <h1 className='px-20 text-3xl text-center md:text-left'>Stack</h1>
         <div className="grid grid-cols-2 gap-3 p-8 md:gap-6 md:py-6 md:px-20 md:w-[40%]">
-            <div className="bg-blue-400 px-3 py-3 flex justify-between items-center rounded-lg">
+            <div className="bg-blue-400 px-3 py-3 flex flex-row lg:justify-between md:w-full items-center rounded-lg">
                 <p className="text-lg text-white">CSS</p>
                 <img src={Css} alt='css'/>
 
             </div>
-            <div className="bg-cyan-200 px-3 py-3 rounded-lg flex justify-between items-center ">
+            <div className="bg-cyan-200 px-3 py-3 rounded-lg flex flex-row md:justify-between items-center ">
                 <p className="text-lg text-white">TAILWIND</p>
                 <img src={Tailwind} alt='tailwind'/>
 
             </div>
-            <div className="bg-green-500 px-3 py-3 rounded-lg flex justify-between items-center ">
-                <p className="text-lg text-white">JAVASCRIPT</p>
+            <div className="bg-green-500 px-3 py-3 rounded-lg flex flex-row md:justify-between items-center ">
+                <p className="text-lg text-white">JS</p>
                 <img src={Javascript} alt='javascript'/>
 
             </div>
-            <div className="bg-orange-600 px-3 py-3 rounded-lg flex justify-between items-center ">
+            <div className="bg-orange-600 px-3 py-3 rounded-lg flex flex-row md:justify-between items-center ">
                 <p className="text-lg text-white">REACT</p>
                 <img src={React} alt='react'/>
 
             </div>
-            <div className="bg-lime-500 px-3 py-3 rounded-lg flex justify-between items-center ">
+            <div className="bg-lime-500 px-3 py-3 rounded-lg flex flex-row md:justify-between items-center ">
                 <p className="text-lg text-white">HTML</p>
                 <img src={Html} alt='html'/>
 
